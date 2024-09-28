eec8380a77210b659b3c2c225b596c15b0cc7b3f
AhmedMerghani <ahmed-merghani@outlook.com>
2024-09-28 15:39:40 +0400

    Players count and current contract balance

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
 import lottery from './lottery';
+import web3 from './web3'
 import { useState, useEffect } from 'react';
 
 function App() {
   const [manager, setManager] = useState('');
+  const [players, setPlayers] = useState('');
+  const [balance, setBalance] = useState('');
 
   useEffect(() => {
     async function getManagerAccount() {
       const managerAddress = await lottery.methods.manager().call();
+      const players = await lottery.methods.getPlayers().call();
+      const balance = await web3.eth.getBalance(lottery.options.address);
+
       setManager(managerAddress);
+      setPlayers(players);
+      setBalance(balance);
     }
     getManagerAccount();
   }, []);
@@ -16,7 +24,9 @@ function App() {
   return (
     <div>
       <h2>Lottery Contract</h2>
-      <p>This contract is managed by {manager}</p>
+      <p>This contract is managed by {manager}.
+        There are currently {players.length} people entered competing to win {web3.utils.fromWei(balance, 'ether')} ether!
+      </p>
     </div>
   );
 }
