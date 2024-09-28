c87f8b45f019d2315ec0ede57b98715fbbae47e8
AhmedMerghani <ahmed-merghani@outlook.com>
2024-09-28 17:30:52 +0400

    Enter and picking a winner from the lottery project

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [manager, setManager] = useState('');
-  const [players, setPlayers] = useState('');
+  const [players, setPlayers] = useState([]);
   const [balance, setBalance] = useState('');
+  const [value, setValue] = useState('');
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
     async function getManagerAccount() {
@@ -21,12 +23,51 @@ function App() {
     getManagerAccount();
   }, []);
 
+  const onSubmit = async(event) => {
+    event.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+
+    setMessage('Waiting on transaction success...');
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(value, 'ether')
+    });
+
+    setMessage('You have been entered!');
+  }
+
+  const onClick = async () => {
+    const accounts = await web3.eth.getAccounts();
+    setMessage('Waiting on transaction success...');
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0]
+    });
+
+    setMessage('A winner has been picked!');
+  }
+
   return (
     <div>
       <h2>Lottery Contract</h2>
       <p>This contract is managed by {manager}.
         There are currently {players.length} people entered competing to win {web3.utils.fromWei(balance, 'ether')} ether!
       </p>
+      <hr />
+      <form onSubmit={onSubmit}>
+        <h4>Want to try your luck?</h4>
+        <div>
+          <label>Amount of ether to enter</label>
+          <input value={value} onChange={event => setValue(event.target.value)} />
+        </div>
+        <button>enter</button>
+      </form>
+      <hr />
+      <h4>Ready to pick a winner?</h4>
+      <button onClick={onClick}>Pick a winner!</button>
+      <hr />
+      <h1>{message}</h1>
     </div>
   );
 }
